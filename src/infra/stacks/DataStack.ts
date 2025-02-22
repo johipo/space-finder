15e7b10992d6e0a7b753fdd5e0fff6b8c60967fa
João Hipólito <jvhipolito627@gmail.com>
2025-02-22 16:31:34 -0300

    Add Dynamo Table

--- src/infra/stacks/DataStack.ts
+++ src/infra/stacks/DataStack.ts
@@ -1,9 +1,24 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
+import { AttributeType, ITable, Table } from 'aws-cdk-lib/aws-dynamodb';
 import { Construct } from 'constructs';
-
+import { getSuffixFromStack } from '../Utils';
 
 export class DataStack extends Stack{
+
+    public readonly spacesTable: ITable
+
     constructor(scope: Construct, id: string, props?: StackProps) {
         super(scope, id, props)
+
+        const suffix = getSuffixFromStack(this);
+        const tableName = `SpacesTable-${suffix}`;
+
+        this.spacesTable = new Table(this, 'SpacesTable', {
+            partitionKey: {
+                name: `id`,
+                type: AttributeType.STRING
+            },
+            tableName: tableName
+        })
     }
 }
