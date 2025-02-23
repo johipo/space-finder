23273b2c36d64c416588f4666988c61f2eadd207
João Hipólito <jvhipolito627@gmail.com>
2025-02-23 10:48:16 -0300

    Add DynamoDb and httpMethod POST

--- test/launcher.ts
+++ test/launcher.ts
@@ -1,4 +1,11 @@
 import { handler } from "../src/services/spaces/handler";
 
+    process.env.AWS_REGION = "eu-west-1",
+    process.env.TABLE_NAME = "SpacesTable-0277555f1943"
 
-handler({} as any, {} as any);
+    handler({
+        httpMethod: 'POST',
+        body: JSON.stringify({
+            location: 'London'
+        })
+    } as any, {} as any);
